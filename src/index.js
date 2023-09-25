4db389a978911fbbf6a1e1d2aae8510e0b521f56
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2023-09-25 16:01:37 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <HelmetProvider>
       <Provider store={store}>
         <PersistGate loading={null} persistor={peristor}>
-          <BrowserRouter basename="/MoneyGuard">
+          <BrowserRouter basename="/Node_React_Eu2_MoneyGuard">
             <GlobalStyles />
             <App />
           </BrowserRouter>
